dffa3bbf08b3a26fe75a9efd8f307c037f225f0f
Donna <dmadeline2003@gmail.com>
2024-04-15 23:26:26 +0800

    change name collection

--- server/models/student.model.js
+++ server/models/student.model.js
@@ -12,7 +12,7 @@ const Student = new Schema(
     course: requiredString,
     year: requiredString,
   },
-  { collection: 'StudentData' } 
+  { collection: 'student-data' } 
 );
 
 const model = _model("StudentData", Student);
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -11,7 +11,7 @@ const User = new Schema(
     email: { ...requiredString, unique: true },
     password: requiredString,
   },
-  { collection: "UserData" }
+  { collection: "user-data" }
 );
 
 const model = _model("UserData", User);
